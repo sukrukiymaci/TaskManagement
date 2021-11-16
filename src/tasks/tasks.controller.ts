8b8cf6d2ef06e864be8f03d8b2d21f2afadd4683
sukrukiymaci <sukrukiymaci@gmail.com>
2021-11-16 10:21:18 -0800

    Working on patch

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,15 +1,38 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private tasksService: TasksService) {}
+    constructor(private tasksService: TasksService) { }
 
     @Get()
     getAllTasks(): Task[] {
         return this.tasksService.getAllTasks();
+    }
+
+    @Get('/:id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
+
+    @Delete('/:id')
+    deleteTaskById(@Param('id') id: string): void {
+        return this.tasksService.deleteTaskById(id);
+    }
+    //TODO
+    @Patch('/:id')
+    patchTaskById(@Param('id') id: string): void {
+        return this.tasksService.deleteTaskById(id);
     }
 
     @Post()
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,21 @@ export class TasksService {
         return this.tasks;
     }
 
+    getTaskById(id: string): Task {
+        return this.tasks.find((task)=> task.id === id)
+    }
+
+    deleteTaskById(id: string): void {
+        /* This is one way using splice: */
+        
+        // const calc = this.tasks.find((task)=> task.id === id);
+        // let num = this.tasks.indexOf(calc);
+        // this.tasks.splice(num,1);
+
+        /* Another one which could be more useful. both can stay and work fine but no need */
+        this.tasks = this.tasks.filter( (task)=> id !== task.id);
+    }
+    
     createTask(createTaskDto: CreateTaskDto): Task {
         const {title, description} = createTaskDto;
         const task: Task = {
